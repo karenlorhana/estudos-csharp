85b2da3d8e43e94ccb726675ba8b2d386f47a120
Karen Lorhana <karen10lorhana@gmail.com>
2021-12-15 15:34:00 -0300

    orientação a objetos

---
+++ csharp-orientacao-a-objetos/006ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Karen";
+            cliente.CPF = "123.456.789-00";
+            cliente.Profissao = "Desenvolvedora";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp-orientacao-a-objetos/007ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ContaCorrente.TotalContasCriadas);
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+            Console.WriteLine(ContaCorrente.TotalContasCriadas);
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaKaren = new ContaCorrente(867, 86755540);
+            Console.WriteLine(ContaCorrente.TotalContasCriadas);
+
+            Console.ReadLine();
+        }
+    }
+}
